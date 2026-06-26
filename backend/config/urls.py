fd207b8191cc5fea903fab19d5a204ce2d8a00d1
tpprogramacion-unraf <javierunraf@gmail.com>
2026-06-26 10:41:42 -0300

    chore: limpiar archivos de test y example

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,13 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.http import JsonResponse
-
-def version_view(request):
-    return JsonResponse({"version": "test-v3-frontend-debug"})
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
-    path('version/', version_view),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
